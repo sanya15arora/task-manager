e1881adc03ba86647a1bb7d2e7449aa74e6a3e2c
Sanya's Mac <sanya15arora@gmail.com>
2025-03-07 23:10:28 +0530

    Url updated

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,8 @@ const app: Express = express();
 // Middleware
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN || "*",
+        origin: "https://task-manage-three.vercel.app/",
+        credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })

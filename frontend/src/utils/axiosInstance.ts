aa45a190eb69bfd765add60fced4a924488aebbb
Sanya's Mac <sanya15arora@gmail.com>
2025-03-07 23:16:38 +0530

    minor fix

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -4,6 +4,7 @@ import { BASE_URL } from "./apiPaths";
 const axiosInstance: AxiosInstance = axios.create({
     baseURL: BASE_URL,
     timeout: 10000,
+    withCredentials: true,
     headers: {
         "Content-Type": "application/json",
         "Accept": "application/json"
